a0b88d808bf62b757c928c67f54c2e45581f4698
Naved Khan <navedkhan0809@gmail.com>
2021-03-26 12:30:39 +0530

    Added: Arithmetic Computation store in Dictionary UC6

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 #Take three inputs a,b and c
-echo -e "Enter 3 Numbers:/c"
+echo -e "Enter 3 Numbers:\c"
 read a b c
 
 a1=$(($a + $b*$c))
@@ -15,3 +15,12 @@ echo "c+a/b =$a3"
 
 a4=`echo "scale=2;($a%$b) + $c"|bc`
 echo "a%b+c =$a4"
+
+declare -A ARITHMETIC
+
+ARITHMETIC[key1]="$a1"
+ARITHMETIC[key2]="$a2"
+ARITHMETIC[key3]="$a3"
+ARITHMETIC[key4]="$a4"
+
+echo ${ARITHMETIC[@]}
